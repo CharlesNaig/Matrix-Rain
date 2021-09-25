09ea7c1c0e4e3454a442f03d129a328b3bffed05
hatry4 <81008382+hatry4@users.noreply.github.com>
2021-09-25 23:29:28 +0800

    Add files via upload

---
+++ matrix.bat
@@ -0,0 +1,8 @@
+@echo off
+color 0a
+title Matrix Rain
+mode 1000
+:a
+
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%  %random%%random%%random%%random% %random%  %random%   %random%%random%  %random% %random% %random% %random% %random%  %random% %random%%random%
+goto a
